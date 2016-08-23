31ed92a15cdd3982a3dc13ed8d0e8a4b4838aed3
Denys Vuika <denis.vuyka@gmail.com>
2016-08-23 11:29:31 +0100

    Code fixes and improvements
    - update demo package.json with author email address
    - fix package.json repository address
    - updated version to 0.2.0
    - improved ESLINT configuration
    - fixed ESLINT errors for karma config files
    - updated karma configs to most recent version
    - updated unit tests (ESLINT)
    - readme updates
    - added references for ‘ng2-alfresco-core’ and ‘alfresco-js-api’
    - fixed: demo app sometimes won’t load and require page reload
    - gulp updates (ESLINT)
    
    closes #13

--- test/app.js
+++ test/app.js
@@ -5,25 +5,25 @@ var helpers = require('yeoman-test');
 var mockery = require('mockery');
 var os = require('os');
 
-describe('Alfresco component generator', function () {
-  before(function () {
+describe('Alfresco component generator', function() {
+  before(function() {
     mockery.enable({
       warnOnReplace: false,
       warnOnUnregistered: false
     });
   });
 
-  after(function () {
+  after(function() {
     mockery.disable();
   });
 
-  it('can be imported without blowing up', function () {
+  it('can be imported without blowing up', function() {
     var app = require('../app');
     assert(app !== undefined);
   });
 
-  describe('defaults', function () {
-    before(function (done) {
+  describe('defaults', function() {
+    before(function(done) {
       helpers.run(path.join(__dirname, '../app'))
         .inDir(path.join(os.tmpdir(), './temp'))
         .withPrompts({
@@ -40,11 +40,11 @@ describe('Alfresco component generator', function () {
         .on('end', done);
     });
 
-    it('created and CD into a folder named like the component', function () {
+    it('created and CD into a folder named like the component', function() {
       assert.equal(path.basename(process.cwd()), 'component-fake');
     });
 
-    it('creates files', function () {
+    it('creates files', function() {
       var expected = [
         '.travis.yml',
         '.gitignore',
@@ -77,13 +77,13 @@ describe('Alfresco component generator', function () {
       assert.file(expected);
     });
 
-    it('fills the README with project data', function () {
+    it('fills the README with project data', function() {
       assert.fileContent('README.md', 'component-fake');
       assert.fileContent('README.md', 'A awesome angular 2 component');
       assert.fileContent('README.md', 'https://github.com/componentCreatorAccount/component-fake/releases');
     });
 
-    it('fills the package.json with project data', function () {
+    it('fills the package.json with project data', function() {
       assert.fileContent('package.json', '"name": "component-fake"');
       assert.fileContent('package.json', '"author": "Alfresco Team <Sonikku.Hejjihoggu@alfresco.com>"');
       assert.fileContent('package.json', '"description": "A awesome angular 2 component"');
@@ -92,40 +92,40 @@ describe('Alfresco component generator', function () {
       assert.fileContent('package.json', '"generator-keyword"');
     });
 
-    it('fills the src file with project data', function () {
+    it('fills the src file with project data', function() {
       assert.fileContent('src/component-fake.component.ts', 'class ComponentFake');
       assert.fileContent('src/component-fake.component.ts', 'Hello World Angular 2 component-fake');
     });
 
-    it('fills the test file with project data', function () {
+    it('fills the test file with project data', function() {
       assert.fileContent('src/component-fake.component.spec.ts', 'Hello World Angular 2 component-fake');
       assert.fileContent('src/component-fake.component.spec.ts', 'ComponentFake');
     });
 
-    it('fills the barrel file with project data', function () {
+    it('fills the barrel file with project data', function() {
       assert.fileContent('index.ts', './src/component-fake.component');
     });
 
-    it('fills the demo package.json with project data', function () {
+    it('fills the demo package.json with project data', function() {
       assert.fileContent('demo/package.json', '"name": "component-fake-demo"');
       assert.fileContent('demo/package.json', '"description": "A awesome angular 2 component - Demo"');
-      assert.fileContent('demo/package.json', '"author": "Alfresco Team"');
+      assert.fileContent('demo/package.json', '"author": "Alfresco Team <Sonikku.Hejjihoggu@alfresco.com>"');
       assert.fileContent('demo/package.json', '"component-fake": "file:../"');
     });
 
-    it('fills the demo README with project data', function () {
+    it('fills the demo README with project data', function() {
       assert.fileContent('demo/README.md', 'component-fake - Demo');
     });
 
-    it('fills the demo file with project data', function () {
+    it('fills the demo file with project data', function() {
       assert.fileContent('demo/index.html', 'component-fake Angular 2');
     });
 
-    it('fills the systemjs.config.js file with project data', function () {
+    it('fills the systemjs.config.js file with project data', function() {
       assert.fileContent('demo/systemjs.config.js', '\'component-fake\': \'node_modules/component-fake/dist\'');
     });
 
-    it('fills the main file with project data', function () {
+    it('fills the main file with project data', function() {
       assert.fileContent('demo/src/main.ts', 'component-fake');
     });
   });
